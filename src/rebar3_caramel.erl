4c56347ce70733d19640e68c44208e8ab505132b
Sean Hinde <sean.hinde@mac.com>
2020-10-24 10:50:33 +0200

    Give up on new style compiler plugin - try trad

--- src/rebar3_caramel.erl
+++ src/rebar3_caramel.erl
@@ -15,7 +15,7 @@ init(State) ->
     Provider = providers:create([
             {name, ?PROVIDER},
             {namespace, ?NAMESPACE},
-            {module, rebar3_caramel_compiler},
+            {module, ?MODULE},
             {bare, true},
             {deps, ?DEPS},
             {example, "rebar3 caramel compile"},
@@ -24,13 +24,6 @@ init(State) ->
             {desc, ""}
     ]),
 
-    State1 = rebar_state:add_provider(State, Provider),
+    {ok, rebar_state:add_provider(State, Provider)}.
 
-    %% This adds the new compiler module:
-    State2 = rebar_state:append_compilers(State1, [rebar3_caramel_compiler]),
-    rebar_api:debug("State: ~p~n", [State2]),
-    %% If needing the new compiler module to take precedence over
-    %% other ones (i.e. generating .erl files from another format):
-    %% State2 = rebar_state:append_compilers(State1, [translator_mod]),
-    {ok, State2}.
 

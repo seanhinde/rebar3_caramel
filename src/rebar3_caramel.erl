a00ed161b34abbc99be005ac093b37cfb514fb00
Sean Hinde <sean.hinde@mac.com>
2020-10-24 11:15:11 +0200

    Try again new style plugin with latest rebar3 for better errors

--- src/rebar3_caramel.erl
+++ src/rebar3_caramel.erl
@@ -2,35 +2,16 @@
 
 -export([init/1]).
 
--define(PROVIDER, compile).
--define(NAMESPACE, caramel).
--define(DEPS, [{default, app_discovery}]).
-
 %% ===================================================================
 %% Public API
 %% ===================================================================
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
 
-    Provider = providers:create([
-            {name, ?PROVIDER},
-            {namespace, ?NAMESPACE},
-            {module, rebar3_caramel_compiler},
-            {bare, true},
-            {deps, ?DEPS},
-            {example, "rebar3 caramel compile"},
-            {opts, []},
-            {short_desc, "An example rebar compile plugin"},
-            {desc, ""}
-    ]),
-
-    State1 = rebar_state:add_provider(State, Provider),
-
     %% This adds the new compiler module:
-    State2 = rebar_state:append_compilers(State1, [rebar3_caramel_compiler]),
-    rebar_api:debug("State: ~p~n", [State2]),
+    State1 = rebar_state:append_compilers(State, [rebar3_caramel_compiler]),
     %% If needing the new compiler module to take precedence over
     %% other ones (i.e. generating .erl files from another format):
     %% State2 = rebar_state:append_compilers(State1, [translator_mod]),
-    {ok, State2}.
+    {ok, State1}.
 

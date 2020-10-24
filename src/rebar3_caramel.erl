212d8607ed9b5956375a4212aac18832f49dff81
Sean Hinde <sean.hinde@mac.com>
2020-10-24 10:17:12 +0200

    Add some debug

--- src/rebar3_caramel.erl
+++ src/rebar3_caramel.erl
@@ -10,6 +10,7 @@ init(State) ->
 
     %% This adds the new compiler module:
     State1 = rebar_state:append_compilers(State, [rebar3_caramel_compiler]),
+    rebar_api:debug("State: ~p~n", [State1]),
     %% If needing the new compiler module to take precedence over
     %% other ones (i.e. generating .erl files from another format):
     %% State2 = rebar_state:append_compilers(State1, [translator_mod]),

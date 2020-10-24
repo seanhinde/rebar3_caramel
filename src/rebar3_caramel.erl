bb883f55f8b7da6d8fecc012c9e8137bce607dea
Sean Hinde <sean.hinde@mac.com>
2020-10-24 10:32:06 +0200

    Trying things to get past command 'compile' in namespace 'rebar3_caramel' not found

--- src/rebar3_caramel.erl
+++ src/rebar3_caramel.erl
@@ -2,17 +2,35 @@
 
 -export([init/1]).
 
+-define(PROVIDER, compile).
+-define(NAMESPACE, caramel).
+-define(DEPS, [{default, app_discovery}]).
+
 %% ===================================================================
 %% Public API
 %% ===================================================================
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
 
+    Provider = providers:create([
+            {name, ?PROVIDER},
+            {namespace, ?NAMESPACE},
+            {module, ?MODULE},
+            {bare, true},
+            {deps, ?DEPS},
+            {example, "rebar3 caramel compile"},
+            {opts, []},
+            {short_desc, "An example rebar compile plugin"},
+            {desc, ""}
+    ]),
+
+    State1 = rebar_state:add_provider(State, Provider),
+
     %% This adds the new compiler module:
-    State1 = rebar_state:append_compilers(State, [rebar3_caramel_compiler]),
-    rebar_api:debug("State: ~p~n", [State1]),
+    State2 = rebar_state:append_compilers(State1, [rebar3_caramel_compiler]),
+    rebar_api:debug("State: ~p~n", [State2]),
     %% If needing the new compiler module to take precedence over
     %% other ones (i.e. generating .erl files from another format):
     %% State2 = rebar_state:append_compilers(State1, [translator_mod]),
-    {ok, State1}.
+    {ok, State2}.
 
